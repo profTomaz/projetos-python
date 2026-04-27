2e290f50c24a4d90ff3401bcd757aaee00fa6160
profTomaz <naoroliveira@prof.educacao.sp.gov.br>
2026-04-27 15:57:01 +0000

    Versão 1

--- var_sentinela.py
+++ var_sentinela.py
@@ -1,1 +1,8 @@
+#Exemplo de Uso da Variável Sentinela
 
+while True:
+    comando = input("Digite um Comando - Para Parar Digite: 'Sair' ")
+if comando == "sair":
+ break
+ print(f"Executando: {comando}")
+print("programa Encerrado")

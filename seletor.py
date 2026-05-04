13968f0b53e709b002b914437af776c46dbd8083
profTomaz <naoroliveira@prof.educacao.sp.gov.br>
2026-05-04 14:09:26 +0000

    v2

--- seletor.py
+++ seletor.py
@@ -1,8 +1,10 @@
-#--Seletor de caracteres--
-letra = input("Digite a Letra")
+print("Sistema Seletor de Caracteres🔠")
+print("Escolha uma Letra e veja se está disponivel👀")
+letra = input("Digite a Letra: ").upper()
 if letra == "A":
-  print("Você escolheu a letra A")
+  print("Você escolheu a letra A ✅")
 elif letra == "B":
-  print("Você escolheu a letra B")
+  print("Você escolheu a letra B ✅")
 else:
-  print("Essa opção não está disponível")
+  print("Essa opção não está disponível ❌")
+a

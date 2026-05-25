41a4665efd486e61d7f726e4cd775f9ff250b324
profTomaz <naoroliveira@prof.educacao.sp.gov.br>
2026-05-25 14:21:35 +0000

    v1

---
+++ qualidade.py
@@ -0,0 +1,14 @@
+#Múltiplas Funções -- Exercício Controle de Qualidade --
+def cabecalho():
+    print("\n" + "=" * 30)
+    print("SISTEMA DE QUALIDADE")
+def verificar_status(peso):
+    if peso >= 50 and peso <=100:
+        return "Aprovada"
+    else:
+        return "Reprovada"
+cabecalho()
+peso_item = float(input("Digite o Peso Item em Gramas"))
+status = verificar_status(peso_item)
+print(f"Resultado da Inspeção: {status}")
+print("-" * 30)
